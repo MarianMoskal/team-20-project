fc273d404c51b4ce584418a1e08a4de82a2b700d
garymiller <77537560+repro-tetris@users.noreply.github.com>
2021-04-03 14:32:38 +0300

    додає зображення для десктопу

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"./..\\images\\problems\\fire.svg":[["fire.f4492d6a.svg","images/problems/fire.svg"],"images/problems/fire.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./sass/main.scss");
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52752" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53359" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
